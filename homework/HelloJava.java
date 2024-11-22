6d1853800b97435e53cbd9a57377770aab9ad56b
HwangEojin <jhin.h8@gmail.com>
2024-11-22 20:34:30 +0900

    20241122 20시33분Java 복습 후 과제 제출

---
+++ homework/HelloJava.java
@@ -0,0 +1,7 @@
+package homework; // 패키지명 homework
+
+public class HelloJava { //클래스 HelloJava
+	public static void main(String[] args) { //Entry Point main 메소드
+		System.out.println("HelloJava"); // 괄호 안의 값을 console 에 출력함
+	}//main 메소드 끝
+}//클래스 HelloJava 끝 
