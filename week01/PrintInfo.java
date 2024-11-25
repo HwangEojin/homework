8c6e181bdf18e3c7605b9a93b61fcf06addbf5c1
HwangEojin <jhin.h8@gmail.com>
2024-11-25 09:18:39 +0900

    241125 1주차 주말과제

---
+++ week01/PrintInfo.java
@@ -0,0 +1,7 @@
+package homework; // 패키지명 homework
+
+public class PrintInfo { //클래스 PrintInfo
+	public static void main(String[] args) { //Entry Point main 메소드
+		System.out.println("황어진 26세"); // 괄호 안의 값을 console 에 출력함
+	}//main 메소드 끝
+}//클래스 PrintInfo 끝 
---
+++ week01/PrintInfo2.java
@@ -0,0 +1,9 @@
+package homework; // 패키지명 homework
+
+public class PrintInfo2 {  //클래스 PrintInfo2
+	public static void main(String[] args) { //Entry Point main 메소드
+		System.out.print("2024년 11월 22일"); // 괄호 안의 값을 줄바꿈 없이 console 에 출력함
+		System.out.print(" 20시 23분"); // 괄호 안의 값을 줄바꿈 없이 c console 에 출력함
+	}//main 메소드 끝
+
+}//클래스 PrintInfo2 끝 
